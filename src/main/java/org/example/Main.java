42bfa0493438043ee2f4345a5c7a1de386bf686d
seungbeen <seungbin803@gmail.com>
2023-05-08 11:59:19 +0900

    Add: @Getter @Setter를 한 줄에 작성하는 법

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,10 @@ import java.io.Serializable;
 // 1. 모든 필드는 private로 지정하고 게터, 세터 메서드를 제공
 // 2. 기본 생성자(=no-args constructor) 추가
 // 3. Serializable 마커 인터페이스를 구현하여 해당 객체가 직렬화 될 수 있도록 설정
-@Getter
-@Setter
 class Student {
-    private String studentId;
-    private String name;
-    private int age;
+    @Getter @Setter private String studentId;
+    @Getter @Setter private String name;
+    @Getter @Setter private int age;
 }
 public class Main {
     public static void main(String[] args) {
