c81b58587e84853730ed4d6f2e8836953e193185
seungbeen <seungbin803@gmail.com>
2023-05-08 12:03:42 +0900

    Add: @NoArgsConstructor // Student() 생성자가 생김      @AllArgsConstructor  // 선언 순서대로 정리      @RequiredArgsConstructor // 필수로 받아야하는 것

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -10,10 +8,14 @@ import java.io.Serializable;
 // 1. 모든 필드는 private로 지정하고 게터, 세터 메서드를 제공
 // 2. 기본 생성자(=no-args constructor) 추가
 // 3. Serializable 마커 인터페이스를 구현하여 해당 객체가 직렬화 될 수 있도록 설정
+@Getter @Setter
+@NoArgsConstructor // Student() 생성자가 생김
+@AllArgsConstructor  // 선언 순서대로 정리
+@RequiredArgsConstructor // 필수로 받아야하는 것
 class Student {
-    @Getter @Setter private String studentId;
-    @Getter @Setter private String name;
-    @Getter @Setter private int age;
+    @NonNull private String studentId;
+    @NonNull private String name;
+    private int age;
 }
 public class Main {
     public static void main(String[] args) {
