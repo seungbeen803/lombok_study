451c93f0b76dcb37594e1de81c117c2598c94962
seungbeen <seungbin803@gmail.com>
2023-05-08 12:11:41 +0900

    Add: @ToString을 작성하면 System.out.println의 내용이 출력됨

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,10 @@ import java.io.Serializable;
 // 2. 기본 생성자(=no-args constructor) 추가
 // 3. Serializable 마커 인터페이스를 구현하여 해당 객체가 직렬화 될 수 있도록 설정
 @Getter @Setter
-@NoArgsConstructor // Student() 생성자가 생김
+@NoArgsConstructor // Student() 생성자가 생김, 기본생성자, 파라미터가 없음
 @AllArgsConstructor  // 선언 순서대로 정리
 @RequiredArgsConstructor // 필수로 받아야하는 것
+@ToString // 내용을 출력해줌
 class Student {
     @NonNull private String studentId;
     @NonNull private String name;
@@ -19,7 +20,7 @@ class Student {
 }
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        Student s1 = new Student("A1234", "John", 20);
+        System.out.println(s1);
     }
 }
