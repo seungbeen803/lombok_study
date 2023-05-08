02bd9f4991b21b9891e5b34dde28e2068f2a2079
seungbeen <seungbin803@gmail.com>
2023-05-08 11:56:52 +0900

    Add: lombok

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+//자바 Bean으로 사용될 클래스의 조건
+// 1. 모든 필드는 private로 지정하고 게터, 세터 메서드를 제공
+// 2. 기본 생성자(=no-args constructor) 추가
+// 3. Serializable 마커 인터페이스를 구현하여 해당 객체가 직렬화 될 수 있도록 설정
+@Getter
+@Setter
+class Student {
+    private String studentId;
+    private String name;
+    private int age;
+}
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
